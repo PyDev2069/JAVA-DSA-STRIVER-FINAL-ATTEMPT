3d738d2f201adb10f91ae95ba7b48d10e669a311
PyDev2069 <rounakchakraborti499@gmail.com>
2026-04-04 12:25:28 +0530

    Code Update

--- sorting/basic_sorting/insertion_sort.java
+++ sorting/basic_sorting/insertion_sort.java
@@ -1,7 +1,7 @@
 public class insertion_sort {
     public static void main(String[] args){
         int[] arr = {13,46,24,52,20,9};
-        for(int i=0; i<=arr.length-1; i++){
+        for(int i=1; i<=arr.length-1; i++){
             int j=i;
             while(j>0 && arr[j-1]>arr[j]){
                 //*swapper */
